0071ffd3be87b66c24f5653893a7a53a367b6524
Axel Reyes <rbaxel.f@gmail.com>
2022-04-25 18:47:20 -0500

    agregando atributos de clase (2)

--- app/spiderman.js
+++ app/spiderman.js
@@ -1,5 +1,9 @@
 class Spiderman{
-	constructor(){
-		
+	constructor(name,age,playedBy,numberOfMovies,cinemaStudio){
+		this.name = name;
+		this.age = age;
+		this.playedBy = playedBy;
+		this.numberOfMovies = numberOfMovies;
+		this.cinemaStudio = cinemaStudio;
 	}
 }
