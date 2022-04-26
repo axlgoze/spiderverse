168b0060963af5af8964cb9df01604b9e69f6c8d
Axel Reyes <rbaxel.f@gmail.com>
2022-04-25 20:55:19 -0500

    prueba superada (7)

--- test/spiderMessage.test.js
+++ test/spiderMessage.test.js
@@ -3,7 +3,7 @@ const Spiderman = require('./../app/spiderman')
 describe("Unit Test for Holland method", () =>{
 	test('2) Use the method getInfo()', () =>{
 		//código que queremos usar
-		const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+		const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel studio")
 		//validar__respuesta esperada
 		expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
 	});
