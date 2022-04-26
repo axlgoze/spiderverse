cedbe9cb430236fe40872ff3020aa1ea782b11fd
Axel Reyes <rbaxel.f@gmail.com>
2022-04-25 19:56:21 -0500

    agregando prueba unitaria de metodo tomHolland(5)

---
+++ test/spiderMessage.test.js
@@ -0,0 +1,10 @@
+import Spiderman = require('./../app/spiderman')
+
+describe("Unit Test for Holland method", () =>{
+	test('2) Use the method getInfo()', () =>{
+		//código que queremos usar
+		const tomHolland = new Spiderman("Spiderman MArvel", "25", "Tom Holland", 5, "Marvel")
+		//validar__respuesta esperada
+		expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
+	});
+})
